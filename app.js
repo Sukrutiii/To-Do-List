b1a6fe0451fb665e9d86f00fab60b706c691f19b
Sukruti Thakare <suk613939@gmail.com>
2022-03-09 17:25:25 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
 app.get("/", function(req, res){
    
     let today = new Date();
